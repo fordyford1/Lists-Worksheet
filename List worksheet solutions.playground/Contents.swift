d1ad8081920363c4ba5f29fad853d1167c52d9d5
Anthony Ford <fordanthony250@gmail.com>
2018-01-30 18:26:47 +0000

    finished

--- List worksheet solutions.playground/Contents.swift
+++ List worksheet solutions.playground/Contents.swift
@@ -3,3 +3,32 @@
 import UIKit
 
 var str = "Hello, playground"
+//Question 3d
+func merge(list1: [Int], list2: [Int]){
+    var copy1 = list1
+    var copy2 = list2
+    var list3 = [Int]()
+    while copy1.count > 0 && copy2.count > 0{
+        if copy1[0] < copy2[0]{
+            list3.append(copy1[0])
+            copy1.removeFirst()
+        }
+        else{
+            list3.append(copy2[0])
+            copy2.removeFirst()
+        }
+    }
+    while copy1.count > 0{
+        list3.append(copy1[0])
+        copy1.removeFirst()
+    }
+    while copy2.count > 0{
+        list3.append(copy2[0])
+        copy2.removeFirst()
+    }
+    print(list1)
+    print(list2)
+    print(list3)
+    
+}
+merge(list1: [1,1,3,5,8], list2: [2,2,2,4,9])
